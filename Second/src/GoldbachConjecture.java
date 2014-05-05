99250597a0ffc453a101d5415f265c01bc13daa8
unknown <smcankova@gmail.com>
2014-05-06 02:27:50 +0300

    final

--- Second/src/GoldbachConjecture.java
+++ Second/src/GoldbachConjecture.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class GoldbachConjecture {
-	public static Boolean IsPrime(int n) {
+	public static Boolean isPrime(int n) {
 		for (int i = 2; i < n; i++) {
 			if (n % i == 0) {
 				return false;
@@ -11,33 +13,35 @@ public class GoldbachConjecture {
 
 	}
 
-	public static LinkedList<Integer> goldbach(int number) {
+	public static ArrayList<ArrayList<Integer>> goldbach(int number) {
 		LinkedList<Integer> primeNumbers = new LinkedList<Integer>();
 		for (int i = 2; i < number; i++) {
-			if (IsPrime(i)) {
+			if (isPrime(i)) {
 				primeNumbers.add(i);
 			}
 		}
 		System.out.println(primeNumbers.size());
-
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		for (int j = 0; j < primeNumbers.size(); j++) {
-			for (int k = 0; k < primeNumbers.size(); k++) {
+			for (int k = j; k < primeNumbers.size(); k++) {
 				if ((primeNumbers.get(j) + primeNumbers.get(k)) == number) {
+					ArrayList<Integer> temporary = new ArrayList<Integer>();
+					temporary.add(primeNumbers.get(j));
+					temporary.add(primeNumbers.get(k));
 
+					result.add(temporary);
 				}
 
 			}
-			if ((primeNumbers.get(j) + primeNumbers.get(j + 1)) == number)
-				;
 
 		}
-		return primeNumbers;
+		return result;
 
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println(goldbach(6));
+		System.out.println(goldbach(10));
 	}
 
 }
