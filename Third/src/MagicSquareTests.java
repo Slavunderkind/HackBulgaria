f5c663f1a4197137e934193d2fb6baae6165f09e
Slavunderkind <smcankova@gmail.com>
2014-05-16 14:17:26 +0300

    add tests :)

--- Third/src/MagicSquareTests.java
+++ Third/src/MagicSquareTests.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -11,8 +9,84 @@ public class MagicSquareTests {
 		boolean expected = false;
 		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
 		Boolean actual = new Boolean(MagicSquare.magic_square(matrix));
-		expected = actual;
+		if (expected == actual)
+			;
 
 	}
 
+	@Test
+	public void onesMatrix() {
+		boolean expected = false;
+		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		a.add(1);
+		a.add(0);
+		a.add(0);
+		b.add(0);
+		b.add(1);
+		b.add(0);
+		c.add(0);
+		c.add(0);
+		c.add(1);
+		matrix.add(a);
+		matrix.add(b);
+		matrix.add(c);
+		Boolean actual = new Boolean(MagicSquare.magic_square(matrix));
+		if (expected == actual)
+			;
+
+	}
+
+	@Test
+	public void mustBeFalse() {
+		boolean expected = false;
+		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		a.add(3);
+		a.add(6);
+		a.add(6);
+		b.add(0);
+		b.add(1);
+		b.add(2);
+		c.add(8);
+		c.add(5);
+		c.add(1);
+		matrix.add(a);
+		matrix.add(b);
+		matrix.add(c);
+		Boolean actual = new Boolean(MagicSquare.magic_square(matrix));
+		if (expected == actual)
+			;
+
+	}
+	
+	@Test
+	public void mustBeTrue () {
+		boolean expected = true;
+		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<Integer> b = new ArrayList<Integer>();
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		a.add(23);
+		a.add(28);
+		a.add(21);
+		b.add(22);
+		b.add(24);
+		b.add(26);
+		c.add(27);
+		c.add(20);
+		c.add(25);
+		matrix.add(a);
+		matrix.add(b);
+		matrix.add(c);
+		Boolean actual = new Boolean(MagicSquare.magic_square(matrix));
+		if (expected == actual)
+			;
+	}
+
 }
+
